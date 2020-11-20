648ccf7a076f5958f7f209881b1b3acbe58e7b1f
Yusuf-Humain <baklaci@humain-labs.com>
2020-11-20 14:20:36 +0100

    eact-zoom-pan-pinch proof-of-concept

--- src/components/views/Room.tsx
+++ src/components/views/Room.tsx
@@ -1,25 +1,40 @@
-import React, { useEffect, useRef, useState } from 'react';
-import ConnectionStore from '../../stores/ConnectionStore';
-import RoomStore from '../../stores/RoomStore';
-import { conferenceName, conferenceOptions, jitsiInitOptions } from '../connection/options';
-import User from './User';
+import React, { useEffect, useRef, useState } from "react";
+import ConnectionStore from "../../stores/ConnectionStore";
+import RoomStore from "../../stores/RoomStore";
+import {
+  conferenceName,
+  conferenceOptions,
+  jitsiInitOptions,
+} from "../connection/options";
+import User from "./User";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
-const Room:React.FC = (props) => {
-  const connectionStore = ConnectionStore.useStore()
-  const roomStore = RoomStore.useStore()
-  useEffect(()=>{
-    if(connectionStore.connected)roomStore.joinRoom("conference2")
-  },[connectionStore.connected])
+const Room: React.FC = (props) => {
+  const connectionStore = ConnectionStore.useStore();
+  const roomStore = RoomStore.useStore();
+  useEffect(() => {
+    if (connectionStore.connected) roomStore.joinRoom("conference2");
+  }, [connectionStore.connected]);
   return (
-    <div>
-      {/* {console.log("Room.roomStore:",roomStore)} */}
-      {roomStore.room&&roomStore.room.egoUser&&<User className="ego" {...roomStore.room.egoUser}/>}
-      {roomStore.room && Object.keys(roomStore.room.users).map((id) => 
-            <User key={id} {...roomStore.room.users[id]}/>
-      )}
-    </div>
-  )
-}
+    //This transformer works when the child (room) size is bigger than the parent (browser window)
+    <TransformWrapper>
+      <TransformComponent>
+        <div className="room">
+          {/* {console.log("Room.roomStore:",roomStore)} */}
+          {roomStore.room && roomStore.room.egoUser && (
+            <User className="ego" {...roomStore.room.egoUser} />
+          )}
+          {roomStore.room && roomStore.room.egoUser && (
+            <User className="ego" {...roomStore.room.egoUser} />
+          )}
+          {roomStore.room &&
+            Object.keys(roomStore.room.users).map((id) => (
+              <User key={id} {...roomStore.room.users[id]} />
+            ))}
+        </div>
+      </TransformComponent>
+    </TransformWrapper>
+  );
+};
 
-
-export default Room
+export default Room;
