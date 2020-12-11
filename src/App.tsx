298c5201b339b8d9af65d608fb4351802fcc68e9
Yusuf-Humain <baklaci@humain-labs.com>
2020-12-11 16:38:32 +0100

    fix: panning wasn't working if scale<=1

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,43 @@ import { Settings } from "./components/Settings/Settings";
 import { Users } from "./components/User/Users";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Room } from "./components/Room/Room";
+import { PropsList } from "react-zoom-pan-pinch/dist/store/interfaces/propsInterface";
+
+const transformWrapperOptions: PropsList = {
+  wheel: { step: 50 },
+  scale: 1,
+  defaultPositionX: 0,
+  defaultPositionY: 0,
+  positionX:0,
+  positionY:0,
+  options: { centerContent: false, limitToBounds: true, limitToWrapper: true, 
+    minScale:0.2, 
+    // maxPositionX:10000, maxPositionY:10000, 
+    // minPositionX:0, minPositionY:0
+  },
+  // scalePadding:{animationTime:10},
+  pan: { velocityEqualToMove: true },
+  pinch: { disabled: true },
+};
 
 function App() {
   return (
-    <div className="App">
-      <Header>Chatmosphere</Header>
-      <JitsiConnection />
-      <LocalStoreLogic />
-      {/* https://github.com/prc5/react-zoom-pan-pinch#zoomin-prop-elements */}
-      <TransformWrapper wheel={{step:50}} scale={2} defaultPositionX={0} defaultPositionY={0} options={{centerContent:false,limitToBounds:false,limitToWrapper:true}}>
-        <TransformComponent>
+      <div className="App">
+        <Header>Chatmosphere</Header>
+        <JitsiConnection />
+        <LocalStoreLogic />
+    <TransformWrapper {...transformWrapperOptions}>
+        {/* https://github.com/prc5/react-zoom-pan-pinch#zoomin-prop-elements */}
+        <TransformComponent >
           <Room>
             <Users />
             <Localuser />
           </Room>
         </TransformComponent>
-      </TransformWrapper>
-      <Settings />
-      <Footer />
-    </div>
+        <Settings />
+        <Footer />
+    </TransformWrapper>
+      </div>
   );
 }
 
