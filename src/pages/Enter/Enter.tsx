ce058d2f9c749b2bf7d65aacc34104a331316991
Yusuf-Humain <baklaci@humain-labs.com>
2021-01-15 13:22:36 +0100

    fix: ending call goes to wrong url "enter/conference1"

--- src/pages/Enter/Enter.tsx
+++ src/pages/Enter/Enter.tsx
@@ -6,9 +6,18 @@ import { Room } from "../../components/Room/Room"
 import { Footer } from "../../components/Footer/Footer"
 import { PanWrapper } from "../../components/PanWrapper/PanWrapper"
 import { LocalUserContainer } from "../../components/Localuser/LocalUserContainer"
+import {useParams} from 'react-router-dom'
+import { useConferenceStore } from "../../store/ConferenceStore"
+import { useEffect } from "react"
 
 export const Enter = () => {
+  const {id} = useParams() //get Id from url, should error check here I guess
+  const setConferenceName = useConferenceStore(state => state.setConferenceName)
 
+  useEffect(() => {
+    setConferenceName(id)
+  },[id])
+  
   return (
     <>
       <Header>Chatmosphere</Header>
