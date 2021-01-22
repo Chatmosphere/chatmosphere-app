a4e1379def2708fed95c1724db10d6bf50e57bb0
Yusuf-Humain <baklaci@humain-labs.com>
2021-01-22 14:36:51 +0100

    fix: Firefox Blocking autoplayed audio

--- src/components/User/AudioTrack.tsx
+++ src/components/User/AudioTrack.tsx
@@ -1,9 +1,13 @@
 import React, { useCallback, useEffect, useRef } from 'react';
+import { useLocalStore } from '../../store/LocalStore';
 import { useConferenceStore } from './../../store/ConferenceStore';
 
 export const AudioTrack = ({id, volume}) => {
   const audioTrack = useConferenceStore(useCallback(store => store.users[id]['audio'], [id]))
   const myRef:any = useRef()
+
+  const localAudioTrack = useLocalStore((store) => store.audio)
+
 
   useEffect(() => {
     myRef.current.volume = volume
@@ -17,6 +21,15 @@ export const AudioTrack = ({id, volume}) => {
       audioTrack?.dispose()
     })
   },[audioTrack])
+
+  //On Firefox, if the media permissions are "Allowed Temporarily" ("Remember this decision" is unchecked in permission prompt);
+  //then Autoplay will be set to Block Audio; resulting the audio of other users not to be heard.
+  //To overcome this problem, we reattach remote audio track on permission is granted. If permission is granted, localAudioTrack is available; so we track its object reference.
+  useEffect(() => {
+    const currentElement = myRef.current
+    audioTrack?.detach(currentElement)
+    audioTrack?.attach(currentElement)
+  },[localAudioTrack])
 
   return (
     <audio autoPlay={true} ref={myRef} className={`remoteTrack audioTrack ${id}audio`} id={`${id}audio`} />
