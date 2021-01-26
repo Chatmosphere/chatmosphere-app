740a6ba48f09df3f104159b1ae29bb727d0de351
David Grieshammer <dkgrieshammer@gmail.com>
2021-01-26 18:35:43 +0100

    Adjusted Mute Button

--- src/components/Footer/MuteButton/MuteButton.tsx
+++ src/components/Footer/MuteButton/MuteButton.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from './../../common/Buttons/Button';
 import {FaMicrophoneSlash, FaMicrophone} from 'react-icons/fa'
 import { useLocalStore } from './../../../store/LocalStore';
 
 
+
 export const MuteButton = () => {
 
-  const {toggleMute, mute} = useLocalStore()
-
-  const handleClick = () => {
-    toggleMute()
-  }
+  const toggleMute = useLocalStore(store => store.toggleMute)
+  const mute = useLocalStore(store => store.mute)
 
   if(mute) {
-    return <Button type="danger" onClick={handleClick}><FaMicrophoneSlash/> Unmute</Button>
+    return <Button type="danger" onClick={toggleMute}><FaMicrophoneSlash/> Unmute</Button>
   } else {
-    return <Button type="secondary" onClick={handleClick}><FaMicrophone/> Mute</Button>
+    return <Button type="secondary" onClick={toggleMute}><FaMicrophone/> Mute</Button>
   }
 }
