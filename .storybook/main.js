6e2469fd749607d49be83fc99738379de6f76233
David Grieshammer <dkgrieshammer@gmail.com>
2022-02-17 18:36:52 +0100

    Updated Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "themeprovider-storybook/register"],
   core: {
     builder: "webpack5"
   }
